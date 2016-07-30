5619ed9b6d190942c2a093a884e79e51c64356bd
YuIzawa <yuizalp@gmail.com>
2016-07-30 23:46:33 +0900

    update

--- src/sinatra_image.rb
+++ src/sinatra_image.rb
@@ -3,9 +3,11 @@ require 'nokogiri'
 require 'sinatra'
 require 'robotex'
 
-get '/view_image' do
+get '/view_image' do 
   user_agent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.63 Safari/537.36'
-  url = 'http://blog.livedoor.jp/anime_info555/archives/1059025536.html'
+  # url = 'http://blog.livedoor.jp/anime_info555/archives/1059025536.html'
+  # url = 'http://aikatunews.livedoor.biz/archives/55581396.html'
+  url = 'http://blog.livedoor.jp/anime_info555/archives/1059638437.html'
   charset = nil
   html = open(url, "User-Agent" => user_agent) do |f|
     charset = f.charset
@@ -14,12 +16,12 @@ get '/view_image' do
 
   doc = Nokogiri::HTML.parse(html, nil, charset)
 
-  html = "<h1>NEWGAME! キャプチャ</h1>"
+  html = "<h1>キャプチャ表示ページ</h1>"
 
   n=0
   doc.css('img').each do |image|
     n += 1
-    html << "<a href=#{image.attribute('src').value}><img src=#{image.attribute('src').value}></a>"
+    html << "<a href=#{image.attribute('src').value}><img src=#{image.attribute('src').value}></a>\n"
   end
 
   return html
