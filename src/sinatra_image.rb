0a8ed0b00c554593d45d78cc198cf7012903234d
YuIzawa <yuizalp@gmail.com>
2016-08-02 22:25:25 +0900

    Fix image crawler

--- src/sinatra_image.rb
+++ src/sinatra_image.rb
@@ -7,7 +7,8 @@ get '/view_image' do
   user_agent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.63 Safari/537.36'
   # url = 'http://blog.livedoor.jp/anime_info555/archives/1059025536.html'
   # url = 'http://aikatunews.livedoor.biz/archives/55581396.html'
-  url = 'http://blog.livedoor.jp/anime_info555/archives/1059638437.html'
+  # url = 'http://blog.livedoor.jp/anime_info555/archives/1059638437.html'
+  url = 'http://aaieba.livedoor.biz/archives/49214309.html'
   charset = nil
   html = open(url, "User-Agent" => user_agent) do |f|
     charset = f.charset
@@ -18,11 +19,9 @@ get '/view_image' do
 
   html = "<h1>キャプチャ表示ページ</h1>"
 
-  n=0
-  doc.css('img').each do |image|
-    n += 1
-    html << "<a href=#{image.attribute('src').value}><img src=#{image.attribute('src').value}></a>\n"
-  end
+doc.css('img').each do |image|
+   html << "<a href=#{image.attribute('src').value}><img src=#{image.attribute('src').value}></a>\n"
+end
 
   return html
 end
